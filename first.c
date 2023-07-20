00a15cae9338fdd869b35b9b2feb0da4219ebab7
NiIchiShi <ki22-i.dub@nubip.edu.ua>
2023-07-20 13:48:23 +0300

    added file function

--- first.c
+++ first.c
@@ -1,9 +1,10 @@
+Демид Ратушний, [20.07.2023 13:40]
 #include <stdio.h>
 #include <string.h>
 
 #define MAX_SHIPMENTS 100
+#define FILENAME "shipments.txt"
 
-// структура для зберігання інформації про посилку
 typedef struct {
     int shipment_number;
     char date[11];
@@ -11,7 +12,6 @@ typedef struct {
     double weight;
 } Shipment;
 
-// функція для введення даних про посилку
 void input_shipment(Shipment *shipment) {
     printf("Enter shipment number: ");
     scanf("%d", &shipment->shipment_number);
@@ -23,7 +23,6 @@ void input_shipment(Shipment *shipment) {
     scanf("%lf", &shipment->weight);
 }
 
-// функція для виведення даних про посилку
 void print_shipment(Shipment shipment) {
     printf("Shipment number: %d\n", shipment.shipment_number);
     printf("Date: %s\n", shipment.date);
@@ -31,7 +30,6 @@ void print_shipment(Shipment shipment) {
     printf("Weight: %.2f kg\n", shipment.weight);
 }
 
-// функція для виведення даних про всі посилки
 void print_all_shipments(Shipment shipments[], int num_shipments) {
     printf("All shipments:\n");
     for (int i = 0; i < num_shipments; i++) {
@@ -40,7 +38,6 @@ void print_all_shipments(Shipment shipments[], int num_shipments) {
     }
 }
 
-// функція для підрахунку загальної ваги посилок за день
 double total_weight_for_date(Shipment shipments[], int num_shipments, char date[11]) {
     double total_weight = 0.0;
     for (int i = 0; i < num_shipments; i++) {
@@ -53,40 +50,81 @@ double total_weight_for_date(Shipment shipments[], int num_shipments, char date[
     return total_weight;
 }
 
-int main() {
-    Shipment shipments[MAX_SHIPMENTS];
+void save_shipments_to_file(Shipment shipments[], int num_shipments) {
+    FILE *file = fopen(FILENAME, "w");
+    if (file != NULL) {
+        for (int i = 0; i < num_shipments; i++) {
+            fprintf(file, "%d %s %s %.2f\n", shipments[i].shipment_number, shipments[i].date, shipments[i].destination_city, shipments[i].weight);
+        }
+        fclose(file);
+    } else {
+        printf("Error opening file for writing.\n");
+    }
+}
+
+int load_shipments_from_file(Shipment shipments[]) {
+    FILE *file = fopen(FILENAME, "r");
     int num_shipments = 0;
 
+    if (file != NULL) {
+        while (fscanf(file, "%d %s %s %lf\n", &shipments[num_shipments].shipment_number,
+                      shipments[num_shipments].date, shipments[num_shipments].destination_city,
+                      &shipments[num_shipments].weight) == 4) {
+            num_shipments++;
+        }
+        fclose(file);
+    }
+
+    return num_shipments;
+}
+
+void remove_shipment(Shipment shipments[], int num_shipments, int index) {
+    if (index < 0 || index >= num_shipments) {
+        printf("Invalid index.\n");
+        return;
+    }
+
+    for (int i = index; i < num_shipments - 1; i++) {
+        shipments[i] = shipments[i + 1];
+    }
+}
+
+int main() {
+    Shipment shipments[MAX_SHIPMENTS];
+    int num_shipments = load_shipments_from_file(shipments);
+
     while (1) {
-        // виводимо меню
         printf("\n");
         printf("1. Add a new shipment\n");
         printf("2. View all shipments\n");
         printf("3. View shipments for a specific date\n");
-        printf("4. Exit\n");
+        printf("4. Delete a shipment\n");
+        printf("5. Exit\n");
         printf("Enter your choice: ");
 
         int choice;
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1: // додати нову посилку
+Демид Ратушний, [20.07.2023 13:40]
+switch (choice) {
+            case 1:
                 if (num_shipments < MAX_SHIPMENTS) {
                     input_shipment(&shipments[num_shipments]);
                     num_shipments++;
                     printf("Shipment added successfully.\n");
+                    save_shipments_to_file(shipments, num_shipments);
                 } else {
                     printf("Cannot add more shipments, maximum number of shipments reached.\n");
                 }
                 break;
-            case 2: // переглянути всі посилки
+            case 2:
                 if (num_shipments > 0) {
                     print_all_shipments(shipments, num_shipments);
                 } else {
                     printf("No shipments to display.\n");
                 }
                 break;
-            case 3: // пошук посилок за датою та виведення загальної ваги
+            case 3:
                 if (num_shipments > 0) {
                     char date[11];
                     printf("Enter date to search (YYYY-MM-DD): ");
@@ -99,7 +137,23 @@ int main() {
                 }
                 break;
             case 4:
+                if (num_shipments > 0) {
+                    int index;
+                    printf("Enter the index of the shipment to delete: ");
+                    scanf("%d", &index);
+                    remove_shipment(shipments, num_shipments, index);
+                    num_shipments--;
+                    save_shipments_to_file(shipments, num_shipments);
+                    printf("Shipment deleted successfully.\n");
+                } else {
+                    printf("No shipments to delete.\n");
+                }
+                break;
+            case 5:
                 return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
         }
     }
     return 0;
